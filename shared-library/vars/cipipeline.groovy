1157a5a38441c47270619af5650c286b5cade4f7
Raghu K <raghuk.vit@gmail.com>
2023-09-16 06:43:56 +0530

    Terraform

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -33,7 +33,7 @@ def call() {
 
       stage('Release') {
         when {
-          expression { TAG_NAME ==~ ".*" }
+          expression { env.TAG_NAME ==~ ".*" }
         }
         steps {
           sh 'env'
