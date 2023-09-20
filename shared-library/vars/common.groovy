41bfe8a3bb090ae0dc355db49a8ea3f2ba6806c1
Raghu K <raghuk.vit@gmail.com>
2023-09-20 06:57:32 +0530

    Changes

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -38,8 +38,8 @@ def test() {
 
 def codeQuality() {
   stage('Code Quality') {
-    sonaruser = sh (script: 'aws ssm get-parameter --name "sonarqube.user" --with-decryption --query="Parameter.Value"', returnStatus: true)
-    sonarpass = sh (script: 'aws ssm get-parameter --name "sonarqube.pass" --with-decryption --query="Parameter.Value"', returnStatus: true)
+    sonaruser = sh (script: 'aws ssm get-parameter --name "sonarqube.user" --with-decryption --query="Parameter.Value"', returnStdout: true).trim()
+    sonarpass = sh (script: 'aws ssm get-parameter --name "sonarqube.pass" --with-decryption --query="Parameter.Value"', returnStdout: true).trim()
     sh "sonar-scanner -Dsonar.host.url=http://172.31.89.117:9000 -Dsonar.login=${sonaruser} -Dsonar.password=${sonarpass} -Dsonar.projectKey=${component} -Dsonar.qualitygate.wait=true"
   }
 }
