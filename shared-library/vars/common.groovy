8147b1101acdaf8ecb4a4bcc54a801cd64cf3ef4
Raghu K <raghuk.vit@gmail.com>
2023-09-22 06:23:14 +0530

    Changes

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -63,8 +63,8 @@ def codeSecurity() {
 
 def release() {
   stage('Release') {
-    env.nexususer = sh (script: 'aws ssm get-parameter --name "nexus.user" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
-    env.nexuspass = sh (script: 'aws ssm get-parameter --name "nexus.pass" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
+    env.nexususer = sh (script: 'aws ssm get-parameter --name "nexus.username" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
+    env.nexuspass = sh (script: 'aws ssm get-parameter --name "nexus.password" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
     wrap([$class: "MaskPasswordsBuildWrapper", varPasswordPairs: [[password: nexuspass]]]) {
       if(env.codeType == "nodejs") {
         sh 'zip -r ${component}.zip server.js node_modules'
