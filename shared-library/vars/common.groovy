4de37b9b7e76dcba340d65d17395c1197d0bce84
Raghu K <raghuk.vit@gmail.com>
2023-09-22 06:43:02 +0530

    Changes

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -69,6 +69,9 @@ def release() {
       if(env.codeType == "nodejs") {
         sh 'zip -r ${component}-${TAG_NAME}.zip server.js node_modules'
       }
+      if(env.codeType == "maven") {
+        sh 'cp target/${component}-1.0.jar ${component}.jar; zip -r ${component}-${TAG_NAME}.zip ${component}.jar'
+      }
 
       sh 'curl -v -u ${nexususer}:${nexuspass} --upload-file ${component}-${TAG_NAME}.zip http://172.31.24.24:8081/repository/${component}/${component}-${TAG_NAME}.zip'
     }
