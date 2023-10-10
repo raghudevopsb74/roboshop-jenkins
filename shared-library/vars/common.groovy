93bb7f24bd50c82a0769619a1c269812a1dc461f
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-10-10 07:13:27 +0530

    Changes

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -61,22 +61,34 @@ def codeSecurity() {
   }
 }
 
+// THis is for Nexus
+//def release() {
+//  stage('Release') {
+//    env.nexususer = sh (script: 'aws ssm get-parameter --name "nexus.username" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
+//    env.nexuspass = sh (script: 'aws ssm get-parameter --name "nexus.password" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
+//    wrap([$class: "MaskPasswordsBuildWrapper", varPasswordPairs: [[password: nexuspass]]]) {
+//      sh 'echo ${TAG_NAME} >VERSION'
+//      if(env.codeType == "nodejs") {
+//        sh 'zip -r ${component}-${TAG_NAME}.zip server.js node_modules VERSION ${schemadir}'
+//      } else if(env.codeType == "maven") {
+//        sh 'cp target/${component}-1.0.jar ${component}.jar; zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION ${schemadir}'
+//      } else {
+//        sh 'zip -r ${component}-${TAG_NAME}.zip *'
+//      }
+//
+//      sh 'curl -v -u ${nexususer}:${nexuspass} --upload-file ${component}-${TAG_NAME}.zip http://172.31.24.24:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+//    }
+//  }
+//}
+
+// THis is for AWS ECR
 def release() {
   stage('Release') {
-    env.nexususer = sh (script: 'aws ssm get-parameter --name "nexus.username" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
-    env.nexuspass = sh (script: 'aws ssm get-parameter --name "nexus.password" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
-    wrap([$class: "MaskPasswordsBuildWrapper", varPasswordPairs: [[password: nexuspass]]]) {
-      sh 'echo ${TAG_NAME} >VERSION'
-      if(env.codeType == "nodejs") {
-        sh 'zip -r ${component}-${TAG_NAME}.zip server.js node_modules VERSION ${schemadir}'
-      } else if(env.codeType == "maven") {
-        sh 'cp target/${component}-1.0.jar ${component}.jar; zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION ${schemadir}'
-      } else {
-        sh 'zip -r ${component}-${TAG_NAME}.zip *'
-      }
-
-      sh 'curl -v -u ${nexususer}:${nexuspass} --upload-file ${component}-${TAG_NAME}.zip http://172.31.24.24:8081/repository/${component}/${component}-${TAG_NAME}.zip'
-    }
+    sh '''
+      aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 633788536644.dkr.ecr.us-east-1.amazonaws.com
+      docker build -t 633788536644.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME} .
+      docker push 633788536644.dkr.ecr.us-east-1.amazonaws.com/${component}:${TAG_NAME}
+'''
   }
 }
 
